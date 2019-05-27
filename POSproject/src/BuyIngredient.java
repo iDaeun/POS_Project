fdef9c0a2d70491cf584ec59b0f83f6e25bcbc45
orongee22 <j35148@gmail.com>
2019-05-27 19:49:34 +0900

    테스트용 BuyIngredient 업로드

---
+++ POSproject/src/BuyIngredient.java
@@ -0,0 +1,44 @@
+
+
+public abstract class BuyIngredient{
+	int ingPrice; // 구매할 재료 원가격
+	String ingName; // 구매할 재료 이름
+	int ingNum; // 구매할 재료 수량
+	
+	public BuyIngredient() {}
+	
+
+	void setIngredient(String name, int price) {
+		this.ingName = name;
+		this.ingPrice = price;
+	}
+	
+	void buying(String name, int num) {
+	}
+//	
+//	public BuyIngredient(String name, int price) {
+//		this.name = name;
+//		this.price = price;
+//	}
+//	private void setPrice(String n) {
+//		name = name;
+//	}
+//	
+//	public String getName() {
+//		return name;
+//	}
+//	private void setPrice(int p) {
+//		price = p;
+//	}
+//	
+//	public int getPrice() {
+//		return price;
+//	}
+//	
+	
+	// 알림창에서 구입하는 메소드.
+//	void buying(int num) {	
+//		int numOfIng = inginfo.getNum();		
+//		numOfIng += num;		
+//	}
+}
