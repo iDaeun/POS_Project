07d91561f248dcd7b02b973cb941f9544351cec7
soo74 <heuisu74@daum.net>
2019-05-28 09:20:52 +0900

    수정

--- POSproject/src/MenuInfo.java
+++ POSproject/src/MenuInfo.java
@@ -1,71 +1,51 @@
-
 public class MenuInfo {
-		  
-		   String name;
-		   int price;
-		  
-		  public MenuInfo(String name, int price) {
-			  this.name = name;
-			  this.price = price;
-		  }
-		  
-		  public MenuInfo(String name) {
-			  this.name = name;
-		  }
-		  
-	/*
-	 * public MenuInfo(int number,String name) { this.name = name; this.number =
-	 * number; }
-	 */
-		  
 
-		public String getName() { 
-			  return name;
-			}
-	 
-		  
-		  public void setName() {
-		   this.name = name;
-		  }
-		  
-		  public int getPrice() {
-			  return price;
-		  }
-		  
-		  public void setPrice() {
-			  this.price = price;
-		  }
-		  
-		  
-		  
-			void showInfo() {
-				System.out.println("메뉴이름 :"+name);
-				System.out.println("메뉴가격 :"+price);
-		
-			}
-		  
-		  
+     String name;
+     int price;
 
-		  
-		  
-		  
-		  
-		  
-		  
-		  
-		  
-		  
-		  /*메뉴가 들어왔을때 쓰이는 재료개수
-		     메뉴 주문 -> 메뉴선택 -> 메뉴따라 또는 메뉴 개수마다 쓰이는 재료 개수가 다르다
-		  -> 감소되는 재료개수 나타내는 메서드 (김, 쌀, 단무지)
-		  
-		  void ingredientNum() {
-			  
-			  
-			  
-		  }
-		  */
-		  
-		  
-		  
-		}
+    public MenuInfo(String name, int price) {
+
+     this.name = name;
+     this.price = price;
+    }
+
+    public MenuInfo(String name) {
+     this.name = name;
+    }
+
+    //메뉴명을 조회하므로 매개변수필요x
+  public String getName() { 
+     return name;
+   }
+
+  //메뉴이름 변경
+    public void setName(String name) {
+     this.name = name;
+    }
+
+    public int getPrice() {
+     return price;
+    }
+
+    public void setPrice(int price) {
+     this.price = price;
+
+    }
+
+    
+   /* public String toString() {
+     return String.format("%s %d", name,price);
+    }
+    */
+    
+   void showInfo() {
+
+    System.out.println("메뉴이름 :"+name);
+    System.out.println("메뉴가격 :"+price);
+   }
+
+  }
+
+ 
+
+ 
--- POSproject/src/MenuManager.java
+++ POSproject/src/MenuManager.java
@@ -1,181 +1,199 @@
 import java.util.ArrayList;
-	import java.util.Scanner;
+
+import java.util.Scanner;
 
 public class MenuManager {
-	
-		
-	  ArrayList<MenuInfo> MenuList = new ArrayList<MenuInfo>();
-	  
-	  
-	  MenuInfo[] mi = null;
-	  int index = -1;
-		
-	  
-	  //메뉴관리 선택
-	  void MenuManage() {
-		  
-		  while(true) {
-			  
-			  
-			  System.out.println("원하는 메뉴를 선택해주세요.");
-			  System.out.println(" 1.메뉴 추가\n 2.메뉴 수정\n 3.메뉴 삭제\n 4.전체 메뉴 출력\n 5.종료");
-			  
-			 
-			  int choice = Util.sc.nextInt();
-			    
-			  switch(choice) {
-			  case 1:
-				  menuAdd();
-				  break;
-			  case 2:
-				  menuEdit();
-				  break;
-			  case 3:
-				  menuDelete();
-				  break;
-			  case 4:
-				  showAllMenu();
-				  break;
-			  case 5:
-				  System.out.println("메뉴관리를 종료합니다.");
-				  return;
-	  		  
-			  }
-		  }
-	  }
-	  
 
-	  //메뉴이름 찾기
-	  int searchIndex(String name) {
+   ArrayList<MenuInfo> MenuList = new ArrayList<MenuInfo>();
 
-	   //int index = -1;
-	   
-	   //같은 이름을 가지는 메뉴이름을 찾는다
-	   for(int i=0; i<MenuList.size(); i++) {
-	    
-		   
-		   if(MenuList.get(i).getName().equals(name)) {
-		 
-			   index = i;
-		   		}
-	   }
-	   return index;
-	  }
-	  
-	  
-	  
-	  
-	  //메뉴이름입력
-	  void insertMenu() {
-		  String name;
-		  int price;
-		  
-		 
-		  MenuList.add(new MenuInfo("야채김밥", 3000));
-		  MenuList.add(new MenuInfo("참치김밥", 4000));
-		  MenuList.add(new MenuInfo("치즈김밥", 3500));
-		  
-		  System.out.println("메뉴이름을 입력하세요.");
-		  name = Util.sc.nextLine();
-		  System.out.println("메뉴의 가격을 입력하세요.");
-		  price = Util.sc.nextInt();
-		  
-		  MenuList.add(new MenuInfo(name, price));
-		  
-	  }
-	  
-	  
-	  
-	  //신메뉴 추가 메서드
-	  void menuAdd() {
-	   System.out.println("추가할 메뉴의 이름을 입력하세요 : ");
-	   String name = Util.sc.nextLine();
-	   System.out.println("추가할 메뉴의 가격을 입력하세요 : ");
-	   int price = Util.sc.nextInt();
-		 
-	   
-	   int index = searchIndex(name);
-	   MenuList.get(index).price = price;
-	   
-	   if(!(index<0)) {
-	    System.out.println("같은 이름이 존재합니다.\n 다른 이름을 입력해주세요");
-	   }else {
-	    System.out.println("새로운 메뉴이름: "+name); 
-	    System.out.println("가격 :"+price);
-	    
-	   }
-	   
-	   MenuInfo info = new MenuInfo(name,price);
-	   MenuList.add(info);
-	   System.out.printf("%d원인 %s메뉴가 추가되었습니다.",price,name);
-	   
-	   
-	   
-	  }
-	  
-	  //메뉴 수정 메서드
-	  void menuEdit() {
-	   System.out.println("수정할 메뉴의 이름을 입력하세요 : ");
-	   String name = Util.sc.nextLine();
-	   
-	   int index = searchIndex(name);
-	   int price = Util.sc.nextInt();
-	   
-	   
-	   //MenuList.get(index).price = price;
-	   
-	   if(index<0) {
-	    System.out.println("메뉴의 이름이 존재하지 않습니다.");
-	   }else {
-	    System.out.println("수정된 메뉴 이름 : ");
-	    name = Util.sc.nextLine();
-	    System.out.println("수정할 메뉴의 가격을 입력하세요 : " + price);
-	   }
-	   
-	    MenuList.remove(index);
-	    MenuList.add(index, new MenuInfo(name));
-	  }
-	  
-	  
-	  
-	  //메뉴 삭제 메서드
-	  void menuDelete(){
-	   System.out.println("삭제할 메뉴의 이름을 입력하세요 : ");
-	   String name = Util.sc.nextLine();
-	   
-	   int index = searchIndex(name);
-	   
-	   if(index<0) {
-	    System.out.println("메뉴의 이름이 존재하지 않습니다.");
-	   }else {
-	    MenuList.remove(index);
-	    System.out.printf("%s메뉴가 삭제되었습니다.",name);
-	   }
-	  }
-	  
-	
-	  
-	// 배열에 저장된 모든 데이터를 출력
-				void showAllMenu() {
-					
-		
-					System.out.println("=======================");
-					
-					if(MenuList.size()>0) {
-					
-						for(int i=0; i<MenuList.size(); i++) {
-							mi[i].showInfo();
-							System.out.println("------------");
-						}
-					} else {
-						System.out.println("등록된 메뉴정보가 없습니다.");
-					}
-					
-					System.out.println("=======================");
-					
-				}
-	  
-	  
-	   
-	  }
+   
+   //메뉴관리 선택
+   void MenuManage() {
 
+    while(true) {
+     int choice = Util.sc.nextInt();
+   
+     System.out.println("=============================");
+     System.out.println(" 1.메뉴 추가\n 2.메뉴 수정\n 3.메뉴 삭제\n 4.전체 메뉴 출력\n 5.종료");
+     System.out.println("원하는 메뉴를 선택해주세요.");
+    
+
+     switch(choice) {
+
+     case 1:
+      menuAdd();
+      break;
+
+     case 2:
+
+      menuEdit();
+      break;
+
+     case 3:
+
+      menuDelete();
+      break;
+
+     case 4:
+
+      showAllMenu();
+      break;
+
+     case 5:
+
+      System.out.println("메뉴관리를 종료합니다.");
+      return;
+
+     }
+
+    }
+
+   }
+   
+   //메뉴를 배열에 저장!!!!!!!!!!!!!!!!!!!
+    public void firstAdd(MenuInfo name) {
+    MenuList.add(name);
+   }
+  
+   
+   //검색할 메뉴의 이름을 출력!!!!!!!!!!!!!
+   public void searchPrint() {
+    System.out.println("검색할 메뉴를 입력해주세요");
+    String name = Util.sc.nextLine();
+    
+    int index = searchIndex(name);
+    
+    if(index<0) {
+     System.out.println("검색하신 메뉴의 정보가 없습니다.");
+    }else {
+     MenuList.get(index).showInfo();
+    }
+   }
+   
+   
+    //같은 이름을 가지는 메뉴이름을 찾는다!!
+    int searchIndex(String name) {
+   int index = -1;
+   for(int i=0; i<MenuList.size(); i++) {
+    if(MenuList.get(i).getName().equals(name)) {
+     index = i;
+     break;
+    }
+   }
+   return index;
+  }
+
+
+   //메뉴이름등록!!!!!!!!!!!!!!!!!!!!!!!
+   void insertMenu() {
+    
+    System.out.println("메뉴이름 :");
+    String name = Util.sc.nextLine();
+    System.out.println("메뉴가격 :");
+    int price = Util.sc.nextInt();
+
+   MenuList.add(new MenuInfo(name, price));
+   System.out.println("메뉴가 등록되었습니다.");
+
+   
+
+   }
+
+
+   //신메뉴 추가 메서드!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+   
+   void menuAdd() { 
+    System.out.println("추가할 메뉴의 이름을 입력하세요 : ");
+    String name = Util.sc.nextLine();
+    
+    System.out.println("추가할 메뉴의 가격을 입력하세요 : ");
+    int price = Util.sc.nextInt();
+
+
+    int index = searchIndex(name);
+
+    //MenuList.get(index).price = price;
+
+    if(!(index<0)) {
+     System.out.println("같은 이름이 존재합니다.\n 다른 이름을 입력해주세요");
+    }else {
+     
+     System.out.println("새로운 메뉴이름: "+name); 
+     System.out.println("가격 :"+price);
+
+     MenuList.add(new MenuInfo(name, price));
+
+    }
+
+    MenuInfo info = new MenuInfo(name,price);
+    MenuList.add(info);
+    System.out.printf("%d원인 %s메뉴가 추가되었습니다.",price,name);
+
+   }
+
+  
+
+   //메뉴 수정 메서드!!!!!!!!!(가격 수정)
+
+   void menuEdit() {
+    System.out.println("수정할 메뉴의 이름을 입력하세요 : ");
+    String name = Util.sc.nextLine();
+   
+    
+    int index = searchIndex(name);
+ 
+
+    if(index<0) {
+     System.out.println("메뉴의 이름이 존재하지 않습니다.");
+    }else {
+
+     System.out.println("수정된 메뉴 이름 : ");
+     name = Util.sc.nextLine();
+     System.out.println("수정할 메뉴의 가격을 입력하세요 : ");
+     int price = Util.sc.nextInt();
+     System.out.println("수정이 완료되었습니다.");
+     
+     MenuList.remove(index);
+     MenuList.add(index, new MenuInfo(name));
+    }
+    
+
+   }
+
+   //메뉴 삭제 메서드!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+   void menuDelete(){
+   
+    System.out.println("삭제할 메뉴의 이름을 입력하세요 : ");
+    String name = Util.sc.nextLine();
+    
+     int index = searchIndex(name);
+    
+    if(index<0) {
+     System.out.println("메뉴의 이름이 존재하지 않습니다.");
+    }else {
+   for(int i = index;i<MenuList.size();i++) {
+     MenuList.remove(index);
+   }
+     System.out.printf("%s메뉴가 삭제되었습니다.",name);
+    }
+   }
+
+   
+ // 배열에 저장된 모든 데이터를 출력!!
+  void showAllMenu() {
+  System.out.println("=======================");
+
+  if(MenuList.size()>0) {
+   for(int i=0; i<MenuList.size(); i++) {
+   MenuList.get(i).showInfo();
+   //MenuInfo name = MenuList.get(i);
+   System.out.println("------------");
+   }
+   } else {
+     System.out.println("등록된 메뉴정보가 없습니다.");
+    }
+  System.out.println("=======================");
+    }
+
+   }
