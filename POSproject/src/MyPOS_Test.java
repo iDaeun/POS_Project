002ee6e5a2fe8deb503fb8609cde81f5ea8837d1
minjongbaek <minjongbaek@gamil.com>
2019-05-28 23:23:34 +0900

    거래처관리 추가

--- POSproject/src/MyPOS_Test.java
+++ POSproject/src/MyPOS_Test.java
@@ -1,16 +1,17 @@
 
-public class testMyPOS implements Util {
+public class MyPOS_Test implements Util {
 
 	static IngredientManager im = new IngredientManager();
-	static SellManager sm = new SellManager();
+	static SellManager sm1 = new SellManager();
 	static MemberManager mm2 = MemberManager.getManager();
 	static MenuManager mm1 = new MenuManager();
+	static SupplierManager sm2 = new SupplierManager();
 
 	public static void main(String[] args) {
 
 		while (true) {
 			System.out.println("POS");
-			System.out.println("1. 재고, 2. 판매, 3. 메뉴관리, 4. 회원관리");
+			System.out.println("1. 재고, 2. 판매, 3. 메뉴관리, 4. 회원관리, 5. 거래처관리");
 
 			int select = sc.nextInt();
 
@@ -19,7 +20,7 @@ public class testMyPOS implements Util {
 				im.showMenu();
 				break;
 			case 2:
-				sm.selectSellMenu();
+				sm1.selectSellMenu();
 				break;
 			case 3:
 				mm1.printMenu();
@@ -27,7 +28,9 @@ public class testMyPOS implements Util {
 			case 4:
 				mm2.showMenu();
 				break;
-			default:
+			case 5:
+				sm2.printMenu();
+				default:
 				System.out.println("올바르게 입력해\n");
 				break;
 
--- POSproject/src/SupplierBasicInfo.java
+++ POSproject/src/SupplierBasicInfo.java
@@ -4,42 +4,58 @@ public class SupplierBasicInfo extends BuyIngredient{
 	String name; // 거래처 이름
 	String phoneNum; // 거래처 번호
 	String address; // 거래처 주소
-	String addIngre; // 거래처 취급품목
-
-	SupplierBasicInfo(String name, String phoneNum,String address, String addIngre) {
+		
+	SupplierBasicInfo(String name, String phoneNum,String address, String ingName, int ingPrice) {
+		super(ingName,ingPrice);
 		this.name = name;
 		this.phoneNum = phoneNum;
 		this.address = address;
-		this.addIngre = addIngre;
 	}
 	
-	SupplierBasicInfo(String name, String phoneNum, String address){
-		this(name,phoneNum,address,"-");
+	public SupplierBasicInfo(String name, String phoneNum,String address, String ingName) {
+		this.name = name;
+		this.phoneNum = phoneNum;
+		this.address = address;
+		super.ingName = ingName;
 	}
+	
+	public SupplierBasicInfo() {};
+	
+	@Override
+	public boolean equals(Object obj) {
+		
+
+		if(obj != null) {
+			return true; 
+		}
+		
+		SupplierBasicInfo sbi = (SupplierBasicInfo)obj;
+		
+		
+		if(obj != null && sbi.phoneNum == obj) {
+			return true;
+		}
+		
+		if(obj != null && sbi.ingName == obj) {
+			return true;
+		}
+		
+		return false;
+	}
+
+	@Override
+	public int hashCode() {
+		// TODO Auto-generated method stub
+		return phoneNum.hashCode();
+	}
+	
+	
 	
 	void showData(){
 		System.out.println("거래처 이름: "+name);
 		System.out.println("거래처 번호: "+phoneNum);
 		System.out.println("거래처 주소: "+address);
-		System.out.println("거래처 품목: "+addIngre);
+		System.out.println("거래처 품목: "+ingName);
 	}
-	
-	void setIngredient(String name, int price) {
-		
-	}
-	
-	// 거래처 이름
-	protected String getName() {
-		return this.name;
-	}
-	
-	protected String getIngre() {
-		return this.addIngre;
-	}
-	
-//	void buying(String name, int num) {
-//		
-//	}
-	
 	
 }
